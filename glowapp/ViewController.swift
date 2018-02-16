bbb8ebe80dbd7a50374c585a05df5fa25dbf628d
Charles Suarez <charlessuarez@MacBook-Pro-2.local>
2018-02-15 18:29:52 -0600

    hello

---
+++ glowapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  glowapp
+//
+//  Created by Charles Suarez on 2/15/18.
+//  Copyright © 2018 Charles Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
